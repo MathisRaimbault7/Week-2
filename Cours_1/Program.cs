762eb40ffd23ea2689d64710d29d1ecbaa899e01
MathisRaimbault7 <114660649+MathisRaimbault7@users.noreply.github.com>
2022-10-11 11:06:13 +0100

    Add files via upload

---
+++ Cours_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Cours_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mathis";
+            Console.WriteLine("My name is : " + name);
+            Console.WriteLine("My name is : {0}", name);
+
+            Student var = new Student (111, name, 20.0, 'F');
+            Student var2 = new Student(112, name, 20.0);
+            Student var3 = new Student(113, name);
+            Student var4 = new Student();
+
+
+            /*var.userName = 8387;
+            var.name = name;
+            var.score = 20;
+            var.nationality = 'F';
+            */
+
+            var.DisplayDetails();
+            var2.DisplayDetails();
+            var3.DisplayDetails();
+            var4.DisplayDetails();
+
+            var4.Score = 13;
+            double score = var4.Score;
+
+            Console.WriteLine(score+"\n");
+
+            var2.ChangeScore(7);
+            var2.DisplayDetails();
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Cours_1/Student.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cours_1
+{
+    public class Student
+    {
+        public int userName;
+        public string name;
+        public double score;
+        public char nationality;
+
+        public Student(int userName, string name, double score, char nationality='I')
+        {
+            this.userName = userName;
+            this.name = name;
+            this.score = score;
+            this.nationality = nationality;
+
+        }
+
+        public Student():this(120,"Louis",10.0,'S')
+        {
+           
+        }
+
+        public Student(int userName, string name) :this(userName,name,0.0,'E')
+        {
+
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("UserName: " + userName);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Nationality: " + nationality);
+            Console.WriteLine("Score: " + score+"\n");
+        }
+
+        public int UserName
+        {
+            get
+            {
+                if (userName < 200)
+                {
+                    return userName;
+                }
+                else
+                {
+                    Console.WriteLine("Cannot display");
+                    return 0;
+                }
+
+            }
+            set
+            {
+                userName = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public char Nationality
+        {
+            get
+            {
+                return nationality;
+            }
+            set
+            {
+                nationality = value;
+            }
+        }
+
+        public double Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                score = value;
+            }
+        }
+
+        public void ChangeScore(double newScore)
+        {
+            score = newScore;
+        }
+    }
+}
